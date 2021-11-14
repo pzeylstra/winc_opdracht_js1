74733e4d4970535e5455be4b3ff129c24db3bf26
Paméla Zeylstra <pamela@pam-organisatieondersteuning.nl>
2021-11-14 13:59:45 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+let name = 'Pamela';
+console.log(name);
+// Je kunt geen variable hebben met de naam if
+let sum = 4 + 4;
+console.log(sum)
+let sumWithStrings = "4" + "4";
+console.log(sumWithStrings);
+let subtraction = 8 - 3;
+console.log(subtraction);
+let multiplication = 14 * 5;
+console.log(multiplication);
+let division = 20 / 4;
+console.log(division);
+let modulus = 87 % 9;
+console.log(modulus);
+let age = '54';
+console.log(age);
+let ageType = 54;
+console.log(typeof ageType);
